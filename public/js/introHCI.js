c3bf4c754eee5d6ea8fdeb4c02a358ca2ec962fe
SallyPoon <sallypoon22@gmail.com>
2021-02-18 10:47:58 -0800

    Added like clicker

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,13 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$("button.likeBtn").click(projectClick);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    ga("send", "event", "like", "click");
 }
